48c1dc9340623c128a7cec48712f5f8f6c4e88e1
RudroVi91225 <rudrodu91225@gmail.com>
2022-12-30 21:01:02 +0600

    commit

--- post.js
+++ post.js
@@ -5,7 +5,14 @@ function displayPost() {
 }
 
 function display(posts) {
+    const postContainer = document.getElementById('posts');
     for (const post of posts) {
+        const div = document.createElement('div');
+        div.classList.add('post');
+        div.innerHTML = `
+        <h3>${post.title}</h3>
+        <p>${post.body}</p> `;
+        postContainer.appendChild(div);
         console.log(post);
     }
 
